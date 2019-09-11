c8609c4df1e081a861e2c2faad5cffa0c027e4f5
Fabian Guerra <fabian.guerra@mapbox.com>
2019-09-11 13:26:35 -0700

    Initial Commit

---
+++ MapboxSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapboxSwiftUIDemo
+//
+//  Created by Fabian Guerra Soto on 9/11/19.
+//  Copyright © 2019 Mapbox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
